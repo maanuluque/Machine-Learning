0bc9f67f8b6903d6f0089d8f3b5d6c15762457d2
Manu <mluque@itba.edu.ar>
2021-03-08 17:25:58 -0300

    Dfs added

--- Testing/test.py
+++ Testing/test.py
@@ -4,6 +4,8 @@ from node import Node
 from board import Board
 from gameController import GameController
 from NonInformative.bfs import bfs
+from NonInformative.dfs import dfs
+
 from solution import Solution
 import json
 
@@ -13,8 +15,9 @@ with open('testConfig.json') as config_file:
 
 algorithm = data['algorithm']
 game_map = data['map']
-print(algorithm)
-print(game_map)
+print("Chosen algorithm is:", algorithm)
+print("Board:")
+print()
 
 with open(game_map) as game_map_file:
     lines = game_map_file.readlines()
@@ -37,16 +40,28 @@ for x, line in enumerate(lines):
         else:
             board[x][y] = char
 
-print(height)
-print(width)
+
 board = Board(board, height, width)
-board.print_state(player=player, boxes=boxes)
+#board.print_state(player=player, boxes=boxes)
 
 node = Node(player, boxes)
 game = GameController(board)
 
-
-game_solution = bfs(game, node)
-game.print_path(game_solution.path) if game_solution.solved else print("sad")
+if algorithm == "bfs":
+    game_solution = bfs(game, node)
+    if game_solution.solved:
+        game.print_path(game_solution.path)
+        print("Processing time: ", game_solution.processing_time)
+        print("Deepness: ", game_solution.expanded)
+    else:
+        print("Solution not found.")
+elif algorithm == "dfs":
+    game_solution = dfs(game, node)
+    if game_solution.solved:
+        game.print_path(game_solution.path)
+        print("Processing time: ", game_solution.processing_time)
+        print("Deepness: ", game_solution.expanded)
+    else:
+        print("Solution not found.")
 
 
