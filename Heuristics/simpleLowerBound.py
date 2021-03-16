58ab98395e41e7baaa5cd1572407b67f49cd1f99
Lu Karpovich <lkarpovich@itba.edu.ar>
2021-03-16 20:17:02 -0300

    added slb*

--- Heuristics/simpleLowerBound.py
+++ Heuristics/simpleLowerBound.py
@@ -3,14 +3,19 @@ from Heuristics.manhattanDistance import manhattan_dist
 
 def slb(board, boxes, player):
     total_sum = 0
-    dist = 0
+    dist = float('inf')
     for box in boxes:
         for goal in board.goals:
             aux = manhattan_dist(box, goal)
-            dist = aux if dist == 0 or aux < dist else dist
+            dist = aux if aux < dist else dist
         total_sum = total_sum + dist
         dist = 0
     return total_sum
 
+
 def slb_plus(board, boxes, player):
-    pass
+    player_dist = float('inf')
+    for box in boxes:
+        aux = manhattan_dist(player, box)
+        player_dist = aux if aux < player_dist else player_dist
+    return slb(board, boxes, player) + player_dist
