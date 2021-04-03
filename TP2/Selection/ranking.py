f9da11e453a673ecd0eba2bc426cb231f23e4d1f
manuel tarradellas <mtarradellas@itba.edu.ar>
2021-04-03 10:24:52 -0300

    ranking roulette fix

--- TP2/Selection/ranking.py
+++ TP2/Selection/ranking.py
@@ -18,17 +18,22 @@ class Ranking(Selection):
         # Reverse iteration. Formula for Relative Rank Performance is:
         # F = [N - rank(i)] / N
 
-        performances_dict = {}
-        # Accumulated Rank Performance. Key: F, Value: Character
+        # Pseudo fitness
         for index in range(population_size):
-            f = (population_size - index) / population_size # TODO index [0, N] or [1, N]?????
-            self.acc_rank_performances[index] = f
-            performances_dict[f] = ranking[index]
+            f = (population_size - (index+1)) / population_size
+            self.acc_rank_performances[index] = f      
+
+        total_fitness = sum(self.acc_rank_performances)
+        accumulated_performance = 0
+        performances_dict = {}
+        for idx, character in enumerate(population):
+            accumulated_performance += (self.acc_rank_performances[idx] / total_fitness)
+            self.acc_rank_performances[idx] = accumulated_performance
+            performances_dict[accumulated_performance] = character
         self.acc_rank_performances = np.sort(self.acc_rank_performances)
 
- 
         selected_population = []
-        for index in range(self.amount):
+        for _ in range(self.amount):
             rand = random.random()
             winner = np.searchsorted(self.acc_rank_performances, rand)
             selected_population.append(performances_dict[self.acc_rank_performances[winner]])
