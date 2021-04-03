df22e54784f9daa56825fbe386b6968008a6a22b
manuel tarradellas <mtarradellas@itba.edu.ar>
2021-04-02 23:13:13 -0300

    d_tournament max

--- TP2/Selection/d_tournament.py
+++ TP2/Selection/d_tournament.py
@@ -8,7 +8,7 @@ class DeterministicTournament(Selection):
         self.tournament_group_size = tournament_group_size
 
     def select(self, population):
-        population_size = len(population)
+        population_size = self.population_size
 
         if population_size < self.tournament_group_size:
             raise Exception('Tournament group size must be less than population size') from Exception
@@ -17,22 +17,11 @@ class DeterministicTournament(Selection):
         selected_population = []
         temporal_tournament = []
 
-        # while index < population_size:
-        #     for x in range(self.tournament_group_size):
-        #         if (index + x) < population_size:
-        #             temporal_tournament.append(population[index + x])
-        #         break
-        #     index += self.tournament_group_size
-        #     selected_population.append(temporal_tournament.pop())
-        #     temporal_tournament.clear()
-
         for index in range(self.amount):
             for value in range(self.tournament_group_size):
                 rand = random.randint(0,population_size-1)
                 temporal_tournament.append(population[index])
-            temporal_tournament.sort()
-            selected_population.append(temporal_tournament.pop())
+            selected_population.append(max(temporal_tournament))
             temporal_tournament.clear()
 
         return selected_population
-
