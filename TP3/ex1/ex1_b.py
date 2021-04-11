6d44d19a0ac7755075866aee2d1eb318bdfc11f4
manuel tarradellas <mtarradellas@itba.edu.ar>
2021-04-11 20:56:44 -0300

    added xor

--- TP3/ex1/ex1_b.py
+++ TP3/ex1/ex1_b.py
@@ -10,22 +10,28 @@ train_list = [
     [1, 1]
 ]
 expected_list = [
-    -1,
-    -1,
-    -1,
     1,
+    1,
+    -1,
+    -1,
 ]
 
-sp = SimplePerceptron(w, 0, 2, 0.5)
-print('Initial predictions:')
-print('Perceptron:')
+print('LOGICAL XOR:')
+sp = SimplePerceptron(w, 0, 0.1)
+print('Initial Perceptron:')
 print(sp)
+print('Initial predictions:')
 print(f'-1  1 : {sp.predict([-1, 1])}')
 print(f' 1 -1 : {sp.predict([1, -1])}')
 print(f'-1 -1 : {sp.predict([-1, -1])}')
 print(f' 1  1 : {sp.predict([1, 1])}')
-sp.train_list(train_list, expected_list)
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`')
+
+times = 100
+while times:
+    sp.train_list(train_list, expected_list)
+    times -= 1
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 print('Final predictions:')
 print('Perceptron:')
 print(sp)
