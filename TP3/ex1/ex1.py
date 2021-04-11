e17d9acdf726ad9837d471efe4d51ce3a3ca0b95
manuel tarradellas <mtarradellas@itba.edu.ar>
2021-04-11 20:13:45 -0300

    ex1 done (imports not working)

--- TP3/ex1/ex1.py
+++ TP3/ex1/ex1.py
@@ -1,1 +1,33 @@
-print('hi')
+from simpleperceptron import SimplePerceptron
+
+w = [0, 0]
+train_list = [
+    [-1, 1],
+    [1, -1],
+    [-1, -1],
+    [1, 1]
+]
+expected_list = [
+    -1,
+    -1,
+    -1,
+    1,
+]
+
+sp = SimplePerceptron(w1, 0, 2, 0.5)
+print('Initial predictions:')
+print('Perceptron:')
+print(sp)
+print(f'-1  1 : {sp.predict([-1, 1])}')
+print(f' 1 -1 : {sp.predict([1, -1])}')
+print(f'-1 -1 : {sp.predict([-1, -1])}')
+print(f' 1  1 : {sp.predict([1, 1])}')
+sp.train_list(train_list, expected_list)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`')
+print('Final predictions:')
+print('Perceptron:')
+print(sp)
+print(f'-1  1 : {sp.predict([-1, 1])}')
+print(f' 1 -1 : {sp.predict([1, -1])}')
+print(f'-1 -1 : {sp.predict([-1, -1])}')
+print(f' 1  1 : {sp.predict([1, 1])}')
---
+++ TP3/simpleperceptron.py
@@ -0,0 +1,38 @@
+from math import copysign
+
+class SimplePerceptron:
+    def __init__(self, weights, threshold, dimension, learning_rate):   
+        self.weights = weights
+        self.threshold = threshold
+        self.dimension = dimension
+        self.learning_rate = learning_rate
+
+    def predict(self, point):
+        h = 0
+        for idx, coord in enumerate(point):
+            h += coord*self.weights[idx]
+        o = copysign(1, (h - self.threshold))
+        return o
+    
+    def train(self, point, expected):
+        predicted = self.predict(point)
+        for idx, coord in enumerate(point):
+            delta_w = self.learning_rate * (expected - predicted) * coord
+            self.weights[idx] = self.weights[idx] + delta_w 
+        self.threshold = sum(self.weights)
+
+    def predict_list(self, point_list):
+        predicted_list = list()
+        for point in point_list:
+            predicted_list.append(self.predict(point))
+        return predicted_list
+            
+    def train_list(self, point_list, expected_list):
+        for idx, point in enumerate(point_list):
+            self.train(point, expected_list[idx])
+
+    def __str__(self):
+        nl = '\n'
+        op = '{'
+        cl = '}'
+        return f'{op}{nl} weights: {self.weights}{nl} threshold: {self.threshold}{nl} dimension: {self.dimension}{nl} rate: {self.learning_rate}{nl}{cl}'
