68972e8985a7b8070a00c267ecfa35a8bc5b7027
manuel tarradellas <mtarradellas@itba.edu.ar>
2021-04-12 16:28:28 -0300

    imports fix with main

--- TP3/ex1/ex1_b.py
+++ TP3/ex1/ex1_b.py
@@ -1,41 +1,40 @@
-import sys
-sys.path.append(".")
-from simpleperceptron import SimplePerceptron
+from perceptron.simpleperceptron import SimplePerceptron
 
-w = [0, 0]
-train_list = [
-    [-1, 1],
-    [1, -1],
-    [-1, -1],
-    [1, 1]
-]
-expected_list = [
-    1,
-    1,
-    -1,
-    -1,
-]
+def ex1_b():
+    w = [0, 0]
+    train_list = [
+        [-1, 1],
+        [1, -1],
+        [-1, -1],
+        [1, 1]
+    ]
+    expected_list = [
+        1,
+        1,
+        -1,
+        -1,
+    ]
 
-print('LOGICAL XOR:')
-sp = SimplePerceptron(w, 0, 0.1)
-print('Initial Perceptron:')
-print(sp)
-print('Initial predictions:')
-print(f'-1  1 : {sp.predict([-1, 1])}')
-print(f' 1 -1 : {sp.predict([1, -1])}')
-print(f'-1 -1 : {sp.predict([-1, -1])}')
-print(f' 1  1 : {sp.predict([1, 1])}')
+    print('LOGICAL XOR:')
+    sp = SimplePerceptron(w, 0, 0.1)
+    print('Initial Perceptron:')
+    print(sp)
+    print('Initial predictions:')
+    print(f'-1  1 : {sp.predict([-1, 1])}')
+    print(f' 1 -1 : {sp.predict([1, -1])}')
+    print(f'-1 -1 : {sp.predict([-1, -1])}')
+    print(f' 1  1 : {sp.predict([1, 1])}')
 
-times = 100
-while times:
-    sp.train_list(train_list, expected_list)
-    times -= 1
+    times = 100
+    while times:
+        sp.train_list(train_list, expected_list)
+        times -= 1
 
-print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-print('Final predictions:')
-print('Perceptron:')
-print(sp)
-print(f'-1  1 : {sp.predict([-1, 1])}')
-print(f' 1 -1 : {sp.predict([1, -1])}')
-print(f'-1 -1 : {sp.predict([-1, -1])}')
-print(f' 1  1 : {sp.predict([1, 1])}')
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print('Final predictions:')
+    print('Perceptron:')
+    print(sp)
+    print(f'-1  1 : {sp.predict([-1, 1])}')
+    print(f' 1 -1 : {sp.predict([1, -1])}')
+    print(f'-1 -1 : {sp.predict([-1, -1])}')
+    print(f' 1  1 : {sp.predict([1, 1])}')
---
+++ TP3/main.py
@@ -0,0 +1,8 @@
+from ex1.ex1_a import ex1_a
+from ex1.ex1_b import ex1_b
+
+print('EXERCISE 1 A:')
+ex1_a()
+print()
+print('EXERCISE 1 B:')
+ex1_b()
