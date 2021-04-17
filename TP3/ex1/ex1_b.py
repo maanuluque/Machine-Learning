717f106900d3412555c6e4b8db4061b3ab17fc3c
manuel tarradellas <mtarradellas@itba.edu.ar>
2021-04-17 17:34:02 -0300

    ex1b numpy

--- TP3/ex1/ex1_b.py
+++ TP3/ex1/ex1_b.py
@@ -1,31 +1,33 @@
+import numpy as np
+from numpy import ndarray
 from perceptron.simpleperceptron import SimplePerceptron
 
 def ex1_b():
-    w = [0, 0]
-    train_list = [
-        [-1, 1],
-        [1, -1],
-        [-1, -1],
-        [1, 1]
-    ]
-    expected_list = [
+    w: ndarray = np.array([0, 0, 0], dtype=float)
+    train_list: ndarray = np.array([
+        [1, -1, 1],
+        [1, 1, -1],
+        [1, -1, -1],
+        [1, 1, 1]
+    ], dtype=float)
+    expected_list: ndarray = np.array([
         1,
         1,
         -1,
         -1,
-    ]
+    ], dtype=float)
 
     print('LOGICAL XOR:')
     sp = SimplePerceptron(w, 0.1)
     print('Initial Perceptron:')
     print(sp)
     print('Initial predictions:')
-    print(f'-1  1 : {sp.predict([-1, 1])}')
-    print(f' 1 -1 : {sp.predict([1, -1])}')
-    print(f'-1 -1 : {sp.predict([-1, -1])}')
-    print(f' 1  1 : {sp.predict([1, 1])}')
+    print(f'-1  1 : {sp.predict(np.array([1, -1,  1]))}')
+    print(f' 1 -1 : {sp.predict(np.array([1,  1, -1]))}')
+    print(f'-1 -1 : {sp.predict(np.array([1, -1, -1]))}')
+    print(f' 1  1 : {sp.predict(np.array([1,  1,  1]))}')
 
-    times = 100
+    times = 1000
     while times:
         sp.train_list(train_list, expected_list)
         times -= 1
@@ -34,7 +36,7 @@ def ex1_b():
     print('Final predictions:')
     print('Perceptron:')
     print(sp)
-    print(f'-1  1 : {sp.predict([-1, 1])}')
-    print(f' 1 -1 : {sp.predict([1, -1])}')
-    print(f'-1 -1 : {sp.predict([-1, -1])}')
-    print(f' 1  1 : {sp.predict([1, 1])}')
+    print(f'-1  1 : {sp.predict(np.array([1, -1,  1]))}')
+    print(f' 1 -1 : {sp.predict(np.array([1,  1, -1]))}')
+    print(f'-1 -1 : {sp.predict(np.array([1, -1, -1]))}')
+    print(f' 1  1 : {sp.predict(np.array([1,  1,  1]))}')
