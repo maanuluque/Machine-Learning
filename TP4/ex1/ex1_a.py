c5cf8a102e0554d39f091aa954d517223e609c4d
manuel tarradellas <mtarradellas@itba.edu.ar>
2021-05-11 17:47:14 -0300

    kohonen update weight fix

--- TP4/ex1/ex1_a.py
+++ TP4/ex1/ex1_a.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from types import SimpleNamespace as Obj
 from ex1.kohonen import Kohonen
 
@@ -9,9 +10,39 @@ def ex1_a(config):
     countries = csv.values[:,0]
     data = csv.values[:,1:]
 
-    kohonen = Kohonen(config.k, config.init_radius, config.init_lrate, 
-                        config.input_len, config.w_init_mode, data)
+    kohonen = Kohonen(config.k, config.init_radius, config.radius_modifier, config.init_lrate,
+                    config.lrate_modifier, config.input_len, config.w_init_mode, data)
 
-    for itr in range(0, 100):
+    print(f'R: {kohonen.radius} | N: {kohonen.lrate}')
+    for itr in range(0, 1000):
+        # print(f'R: {kohonen.radius} | N: {kohonen.lrate}')
         rand_idx = np.random.randint(config.input_amount)
         kohonen.train(data[rand_idx])
+    print(f'R: {kohonen.radius} | N: {kohonen.lrate}')
+
+    X = []
+    Y = []
+    groups = {}
+    
+    for idx, country in enumerate(countries):
+        w, group = kohonen.predict(data[idx])
+        X.append(group[0])
+        Y.append(group[1])
+        if group not in groups:
+            groups[group] = []
+        groups[group].append(country)
+
+    for group in groups:
+        print(f'Group: {group}:')
+        print(groups[group])
+
+    X = np.array(X)
+    Y = np.array(Y)
+
+    i = 0
+    for group in groups:
+        plt.annotate("".join(groups[group]), (X[i], Y[i]))
+        i += 1
+
+    plt.scatter(X,Y)
+    plt.show()
--- TP4/ex1/kohonen.py
+++ TP4/ex1/kohonen.py
@@ -3,13 +3,15 @@ from numpy import ndarray as arr
 from math import floor, ceil
 
 class Kohonen:
-    def __init__(self, k, radius, lrate, input_len, w_init_mode, inputs:arr=None):
+    def __init__(self, k, radius, radius_modifier, lrate, lrate_modifier, input_len, w_init_mode, inputs:arr=None):
         self.k = k
         self.radius = radius
-        self.lrate = lrate
+        self.init_radius = radius
+        self.radius_modifier = radius_modifier
+        self.lrate = float(lrate)
+        self.lrate_modifier = float(lrate_modifier)
         self.net = self.generate_net(k, input_len, w_init_mode, inputs)
-        self.update_radius = lambda x: x
-        self.update_lrate = lambda x: 1/x
+        self.iter = 0
 
     def generate_net(self, k, input_len, w_init_mode, inputs:arr=None):
         if w_init_mode == "random":
@@ -26,13 +28,21 @@ class Kohonen:
         else:
             raise Exception('Invalid weights initialization mode.')
 
+    def predict(self, inputs: arr):
+        aux = ( inputs - inputs.mean()) / inputs.std()
+        std_inputs = aux / np.linalg.norm(aux)
+        closest_neuron, closest_neuron_idx = self.find_closest_neuron(std_inputs)
+        return closest_neuron, closest_neuron_idx
+
     def train(self, inputs:arr):
-        std_inputs = ( inputs - inputs.mean()) / inputs.std()
+        aux = ( inputs - inputs.mean()) / inputs.std()
+        std_inputs = aux / np.linalg.norm(aux)
         closest_neuron, closest_neuron_idx = self.find_closest_neuron(std_inputs)
         neuron_nhbs = self.find_neuron_neighbours(closest_neuron_idx)
-        self.update_weights(neuron_nhbs, closest_neuron, inputs)
-        self.radius = self.update_radius(self.radius)
-        self.lrate = self.update_lrate(self.lrate)
+        self.update_weights(neuron_nhbs, closest_neuron, std_inputs)
+        self.iter += 1
+        self.radius = 1 + (self.init_radius - 1)*(np.e**(-self.radius_modifier*self.iter))
+        self.lrate = 1/(self.iter*self.lrate_modifier)
         
     def find_closest_neuron(self, inputs):
         closest_neuron: arr = None
@@ -42,7 +52,7 @@ class Kohonen:
         for row in self.net:
             col_i = 0
             for neuron_weights in row:
-                dist = np.linalg.norm(neuron_weights - inputs)
+                dist = np.linalg.norm(inputs - neuron_weights)
                 if dist < closest_distance:
                     closest_neuron = neuron_weights
                     closest_distance = dist
@@ -55,9 +65,9 @@ class Kohonen:
     def find_neuron_neighbours(self, idx):
         neighbours = list()
         row_min = max([0, floor(idx[0] - self.radius)])
-        row_max = min([self.k, ceil(idx[0] + self.radius)])
+        row_max = min([self.k, ceil(idx[0] + self.radius)+1])
         col_min = max([0, floor(idx[1] - self.radius)])
-        col_max = min([self.k, ceil(idx[1] + self.radius)])
+        col_max = min([self.k, ceil(idx[1] + self.radius)+1])
         for row in range(row_min, row_max):
             for col in range(col_min, col_max):
                 if ((row, col) != idx):
@@ -70,7 +80,7 @@ class Kohonen:
     def update_weights(self, weight_list, matched_weights, inputs):
         for weights in weight_list:
             for idx, w in enumerate(weights):
-                weights[idx] = w + self.lrate * (inputs[idx] - matched_weights[idx])
+                weights[idx] = w + self.lrate * (inputs[idx] - w)
 
 
     def __str__(self):
