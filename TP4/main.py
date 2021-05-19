462c4d6a3f64fe0228001c6f0cd16b1630741bd7
Manuel <mluque@itba.edu.ar>
2021-05-19 12:32:49 -0300

    ex2

--- TP4/main.py
+++ TP4/main.py
@@ -1,14 +1,24 @@
 import json
 from types import SimpleNamespace as Obj
+
+import ex2.ex_2_alternative_2
 from ex1.ex1_a import ex1_a
+from ex1.ex1_b import ex1_b
+from ex2.ex_2 import *
+from ex2.ex_2_alternative import *
+from ex2.ex_2_alternative_2 import *
 
 def main():
     with open('config.json') as config_file:
         config = json.load(config_file, object_hook=lambda d: Obj(**d))
 
-    print("Exercise 1:")
+    print("Exercise 1a:")
     ex1_a(config.ex1)
+    print("Exercise 1b:")
+    ex1_b(config.ex1)
 
+    print("Exercise 2:")
+    ex2_a()
 
 
 if __name__ == "__main__":
